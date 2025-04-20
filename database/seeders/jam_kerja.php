b9c85a64e9b99011f8d7b64977d6f8345ef8a89d
Sanwich19 <ihsanwahid1958@gmail.com>
2025-04-20 20:45:22 +0700

    commit isan

---
+++ database/seeders/jam_kerja.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class jam_kerja extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
