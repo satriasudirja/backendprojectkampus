7427c9fa88d55c2ccaf0e54349f307f113f676a0
satriasudirja <satriasadar122@gmail.com>
2025-05-01 00:59:45 +0700

    push

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -36,6 +36,8 @@ class DatabaseSeeder extends Seeder
     SimpegMasterOutputPenelitianSeeder::class,
     SimpegHubunganKerjaSeeder::class,
     SimpegJenisHariSeeder::class,
+    RumpunBidangIlmuSeeder::class,
+    
         ]);
 
     }
