285ca566be150530de2a2c9d69a6021ccc31f7d3
satriasudirja <satriasadar122@gmail.com>
2025-06-06 13:34:33 +0700

    push

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,10 @@ use App\Http\Controllers\CaptchaController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('/captcha/slide-captcha', [CaptchaController::class, 'showSlideCaptcha'])->name('captcha.slide-captcha');
-
-// Route untuk menampilkan gambar captcha dari private storage
-Route::get('/captcha/image/{type}/{id}', [CaptchaController::class, 'serveImage'])->name('captcha.image');
+Route::middleware('web')->group(function () {
+    Route::get('/captcha/slide-captcha', [CaptchaController::class, 'showSlideCaptcha'])->name('captcha.slide-captcha');
+    Route::get('/captcha/image/{type}/{id}', [CaptchaController::class, 'serveImage'])->name('captcha.image');
+});
 
 // // Route untuk dokumentasi Swagger
 // Route::get('/api/documentation', function () {
