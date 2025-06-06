373cf4a8351e94a9b872a0094ae97171cac77f24
satriasudirja <satriasadar122@gmail.com>
2025-06-06 15:16:11 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CaptchaController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'tess';
 });
 Route::get('/test', function () {
     return 'Test route works!';
