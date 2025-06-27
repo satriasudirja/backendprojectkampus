72c3960773bf51052512a8509efaa90375f2eb5d
satriasudirja <satriasadar122@gmail.com>
2025-06-28 01:46:11 +0700

    p

--- database/seeders/SimpegJenisIzinSeeder.php
+++ database/seeders/SimpegJenisIzinSeeder.php
@@ -14,101 +14,98 @@ class SimpegJenisIzinSeeder extends Seeder
             [
                 'kode' => '001',
                 'jenis_izin' => 'Sakit',
-               
+                'jenis_kehadiran_id' => 13,
                 'izin_max' => 3,
                 'potong_cuti' => false
             ],
             [
                 'kode' => '002',
                 'jenis_izin' => 'Izin Menikah',
-               
+                'jenis_kehadiran_id' => 8,
                 'izin_max' => 3,
                 'potong_cuti' => false
             ],
             [
                 'kode' => '003',
                 'jenis_izin' => 'Izin Menikahkan Anak',
-               
+                  'jenis_kehadiran_id' => 8,
                 'izin_max' => 2,
                 'potong_cuti' => false
             ],
             [
                 'kode' => '004',
                 'jenis_izin' => 'Izin Mengkhitankan Anak',
-           
+                  'jenis_kehadiran_id' => 8,
                 'izin_max' => 2,
                 'potong_cuti' => false
             ],
             [
                 'kode' => '005',
                 'jenis_izin' => 'Izin Melahirkan',
-                
+                  'jenis_kehadiran_id' => 8,
                 'izin_max' => 90,
                 'potong_cuti' => false
             ],
             [
                 'kode' => '006',
                 'jenis_izin' => 'Izin Istri Melahirkan',
-              
+                  'jenis_kehadiran_id' => 8,
                 'izin_max' => 2,
                 'potong_cuti' => false
             ],
             [
                 'kode' => '007',
                 'jenis_izin' => 'Izin Anak Sakit',
-   
+                  'jenis_kehadiran_id' => 8,
                 'izin_max' => 2,
                 'potong_cuti' => false
             ],
             [
                 'kode' => '008',
                 'jenis_izin' => 'Izin Kondisi berduka/keluarga wafat',
-  
+                  'jenis_kehadiran_id' => 8,
                 'izin_max' => 2,
                 'potong_cuti' => false
             ],
             [
                 'kode' => '009',
                 'jenis_izin' => 'Izin Ibadah Haji',
- 
+                  'jenis_kehadiran_id' => 8,
                 'izin_max' => 40,
                 'potong_cuti' => false
             ],
             [
                 'kode' => '010',
                 'jenis_izin' => 'Izin Ibadah Umrah',
-    
+                  'jenis_kehadiran_id' => 8,
                 'izin_max' => 14,
                 'potong_cuti' => false
             ],
             [
                 'kode' => '999',
                 'jenis_izin' => 'Izin Lain-lain',
-  
+                  'jenis_kehadiran_id' => 8,
                 'izin_max' => 3,
                 'potong_cuti' => false
             ],
         ];
 
-        foreach ($jenisIzinData as $data) {
-            $jenisKehadiran = SimpegJenisKehadiran::where('nama_jenis', $data['status_presensi'])->first();
+        foreach ($jenisIzinData as $data){
 
-            if (!$jenisKehadiran) {
-                echo "❌ Tidak ditemukan jenis kehadiran: {$data['status_presensi']}\n";
-                continue;
-            }
+       
 
             SimpegJenisIzin::updateOrCreate(
                 ['kode' => $data['kode']],
                 [
-                    'jenis_kehadiran_id' => $jenisKehadiran->id,
+                    'jenis_kehadiran_id' => $data['jenis_kehadiran_id'],
                     'kode' => $data['kode'],
                     'jenis_izin' => $data['jenis_izin'],
-                    
+                
                     'izin_max' => $data['izin_max'],
                     'potong_cuti' => $data['potong_cuti']
                 ]
             );
         }
     }
-}
+
+ }
