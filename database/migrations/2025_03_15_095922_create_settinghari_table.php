bd28c3b3a74e799c7337b1228db35e4c95efe303
wa1t <azkanyan@gmail.com>
2025-08-14 08:54:46 +0700

    ini yang erorr

--- database/migrations/2025_03_15_095922_create_settinghari_table.php
+++ database/migrations/2025_03_15_095922_create_settinghari_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
 {
     Schema::create('settinghari', function (Blueprint $table) {
         $table->uuid('id')->primary(); // Kode int(1) primary ke
-        $table->integer('kode', 1); // Kode int(1) primary key
+        $table->integer('kode', 1); // Kode int(1) primary key ini ganti coba yaa 
         $table->string('nama_hari', 10); // Nama Hari (10)
         $table->string('jenis_hari', 15); // Jenis Hari (15)
         $table->timestamps();
